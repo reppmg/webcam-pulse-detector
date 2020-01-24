cbc35f8023636e21c15864fb7190bb154e933d7f
maksimrepp <reppmg@gmail.com>
2020-01-24 07:58:41 +0300

    flat window

--- experiments.py
+++ experiments.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from test_graph import read_file
 from scipy.signal import savgol_filter
+from smooth import smooth
 
 np.random.seed(1)
 bpm_file = "/Users/maksimrepp/Documents/nir/public_sheet/P1LC5/P1LC5_Mobi_RR-intervals.bpm"
@@ -10,8 +11,8 @@ csv_file = "/Users/maksimrepp/PycharmProjects/webcam-pulse-detector/Webcam-pulse
 
 
 def transform(times, pulse):
-    pulse = savgol_filter(pulse, 51, 3)
-    return times, pulse
+    pulse = smooth(pulse, 11, 'flat')
+    return times, pulse[0:len(times)]
 
 
 data = pd.read_csv(csv_file).to_numpy()
@@ -20,7 +21,7 @@ pulse = data[:, 1]
 times, pulse = transform(times, pulse)
 expected_times, expected_pulse = read_file(bpm_file)
 plt.figure()
-title = "Savitzky-Golay filter"
+title = "Flat window"
 plt.title(title)
 plt.plot(times, pulse, expected_times, expected_pulse)
 # plt.show()
